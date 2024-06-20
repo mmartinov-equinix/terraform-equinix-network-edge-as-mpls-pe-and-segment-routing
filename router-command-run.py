7f7dc2b885c30cd452856c3bbc9d249ab8edf9d4
mmartinov-equinix <132877286+mmartinov-equinix@users.noreply.github.com>
2024-06-20 09:15:30 +0200

    Update router-command-run.py

--- router-command-run.py
+++ router-command-run.py
@@ -6,7 +6,9 @@ from time import sleep
 
 # Establishing connection to each device and configure the content in config file/s.
 def connect_device(device, config_file):
-    host = {"ip":device.strip(), "username":"mmartinov", "device_type":"cisco_ios", "use_keys":True, "key_file":"/Users/martin.martinov/.ssh/martin_key_jun_24", "disable_sha2_fix": True}
+    # Add your SSH username into "username". 
+    # Add absolute path of your SSH key into "key_file".
+    host = {"ip":device.strip(), "username":"your_username", "device_type":"cisco_ios", "use_keys":True, "key_file":"absolute_path_of_ssh_key", "disable_sha2_fix": True}
 
     try:
         net_connect = Netmiko(**host)
@@ -17,20 +19,15 @@ def connect_device(device, config_file):
     except Exception as ex:
         print(f"{device} connection FAILED! -  Error message: ", ex)
 
-# Sometimes it is taking time Terraform output to be ready, this is adding delay to overcome it.
+# Sometimes terraform output is taking time and causing errors, that is why adding delay to overcome it.
 sleep(20)
 
 # Lists the devices created via Terraform
 devices = popen(f"terraform output equinix_network_device_ip_addresses").read()
 
-print(devices)
-
-print(eval(devices)[0])
 # List of the configuration files, in order of devices!
 # In case number is different than 3, please specify config file for each device accordingly!
 config_files = ["r1_config_sr_test.txt", "r2_config_sr_test.txt", "r3_config_sr_test.txt"]
-
-
 
 # Iterating through each device and config file, to push config into this device.
 # eval(devices)[0]    is to parse from string to list and pull only the interesting item in the list.
